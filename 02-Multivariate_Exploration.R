4b4265918af02615a3917189f3d7cb7e75a6b417
amaliavr <amaliavradi@gmail.com>
2019-06-15 16:43:56 +0200

    hierarchical clustering and consolidation

--- 02-Multivariate_Exploration.R
+++ 02-Multivariate_Exploration.R
@@ -7,6 +7,7 @@
 library (cclust)
 library(factoextra)
 library(FactoMineR)
+library(fpc)
 
 # Color palette
 colors <- sample(c('#000000', '#FF0000', '#808000', '#00FF00', '#008000', 
@@ -47,62 +48,93 @@ abline(h=1,lty=2,col="red")
 
 # # first, perform a hierarchical clustering
 
-# # if we perform on clustering on pca, get factorial coordinates of individuals
-# df.pca <- res.pca$ind$coord[, 1:signi]
+# significant dimensions
+signi <- 5
+# if we perform on clustering on pca, get factorial coordinates of individuals
+df.pca <- pca.Happiness$ind$coord[, 1:signi]
 
 # hierarchical clustering on factorial coordinates from PCA
 # calculate a distance matrix between individuals which uses the squared
-
+distances = dist(df.pca, method = "euclidean")
 # # then we apply hclust with method="ward"
-# hward = hclust(distances, method="ward.D2")
+hward = hclust(distances, method="ward.D2")
 
-# # plot dendrograms
-# plot(hward)
+# plot dendrograms
+plot(hward)
 
-# # try number of clusters
-# cl <- ...
-# # cut tree and see the size of clusters
-# c1 <- cutree(hward, cl)
-# table(c1)
-# # Calculate centroids of clusters
-# centroids <- df.pca[1:cl,]
-# row.names(centroids) <- 1:cl
-# for (i in 1:cl) {
-#     for (j in 1:ncol(centroids)) {
-#         centroids[i,j] <- mean(df.pca[which(c1 == i),j])
-#     }
-# }
+clusterData <- function(data, distance.method="euclidean",
+                        cl.method="ward.D2",
+                        min.cl = 2, max.cl = 10, consolidation=FALSE) {
+    
+    #Control parameters
+    if (min.cl > max.cl) {
+        print("Error in cluster min:max interval")
+        return()
+    }
+    
+    # Calculate a distance matrix between individuals 
+    # which uses the squared Euclidean distance
+    distances <- dist(data, method=distance.method)
+    
+    # Perform hierarchical clustering
+    hward <- hclust(distances, method=cl.method)
+    
+    # Initialize storing variables
+    ch <- rep(0, max.cl - min.cl + 1)
+    # Loop for number of clusters cl
+    for (m in 1:(max.cl - min.cl + 1)) {
+        cl <- min.cl + m - 1
+        
+        #print(paste("Number of clusters:", cl))
+        
+        # cut tree and see the size of clusters
+        clust <- cutree(hward, cl)
+        #print(table(clust))
+        
+        if (consolidation) {
+            
+            # Calculate centroids of clusters
+            centroid <- data[1:cl,]
+            row.names(centroid) <- 1:cl
+            for (i in 1:cl) {
+                for (j in 1:ncol(centroid)) {
+                    centroid[i,j] <- mean(data[which(clust == i),j])
+                }
+            }
+            
+            # Perform consolidation
+            cl.kmeans <- kmeans(data, centroid)
+            clust <- cl.kmeans$cluster
+            
+        }
+        
+        # Calculate Caliniski-Harabazi index for clustering
+        ch[m] <- calinhara(data, clust)
+        #print(paste("Calinhara index:", ch[m]))
+        
+    }
+    return(ch)
+    
+}
 
-# # Plot points in first factorial axis with centroids
-# plot(df.pca[,1], df.pca[,2], col=colors[c1],
-#      main="Individuals by cluster group in first factorial plane",
-#      xlab="PC1", ylab="PC2", cex.lab=0.8)
-# plotAddSubtitle(subtitle)
-# abline(v = 0, h = 0, lty = 2)
-# points(centroids, bg = colors[1:cl], cex=2,pch=21, col=colors[1:cl])
+method <- "Ward"
+subtitle <- paste("Aggregation method:", method)
 
-# # Consolidate clusters by doing k-means
-# cl.kmeans <- kmeans(df.pca, centroids)
-# clust <- cl.kmeans$cluster
+min.cl <- 2
+max.cl <- 15
+noConsol <- clusterData(df.pca, min.cl = min.cl, max.cl = max.cl, 
+                        consolidation = FALSE)
+subtitle2 <- paste(subtitle, "Consolidation: No", sep = ", ")
+plot(min.cl:max.cl, noConsol, type = "b",
+     main="Calinski-Harabassz Index vs number of clusters",
+     xlab="Number of clusters", ylab="C-H Index", cex.lab=0.8)
+plotAddSubtitle(subtitle2)
 
-# # Plot points in first factorial axis with centroids
-# plot(df.pca[,1], df.pca[,2], col=colors[clust],
-#      main="Individuals by cluster group in first factorial plane
-#      \nConsolidated clusters",
-#      xlab="PC1", ylab="PC2", cex.lab=0.8)
-# plotAddSubtitle(subtitle)
-# abline(v = 0, h = 0, lty = 2)
-# points(centroids, bg = colors[1:cl], cex=2,pch=21, col=colors[1:cl])
-
-# # Calinski Harabassz index 
-# 
-# ch <- rep(0, 9)
-# for (i in 2:10) {
-#     c2 <- cutree(hward, i)
-#     ch[i-1] <- calinhara(df.pca, c2)
-# }
-# 
-# plot(2:10, ch, type = "b", main="Calinski-Harabassz Index vs number of 
-#      clusters",
-#      xlab="Number of clusters", ylab="C-H Index", cex.lab=0.8)
-# plotAddSubtitle(subtitle)
+yesConsol <- clusterData(df.pca, min.cl = 2, max.cl = 15, 
+                         consolidation = TRUE)
+subtitle3 <- paste(subtitle, "Consolidation: Yes", sep = ", ")
+plot(min.cl:max.cl, yesConsol, type = "b", 
+     main="Calinski-Harabassz Index vs number of clusters
+     \nConsolidated clusters",
+     xlab="Number of clusters", ylab="C-H Index", cex.lab=0.8)
+plotAddSubtitle(subtitle3)
